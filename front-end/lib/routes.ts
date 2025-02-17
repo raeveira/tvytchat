d27b8407526b5cb9e10826a5925ced391046f26e
rae <rae@raeveira.nl>
2025-02-17 23:40:20 +0100

    fixed api auth prefix

--- front-end/lib/routes.ts
+++ front-end/lib/routes.ts
@@ -21,7 +21,7 @@ export const authRoutes = [
  * Routes that start with this prefix are used for API authentication purposes
  * @type {string}
  */
-export const apiAuthPrefix = ""
+export const apiAuthPrefix = "/api/auth"
 
 /*
     * An array of routes that are used for admin purposes
