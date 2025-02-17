cae898de68f3ecd12afcef9cac25257c959fbfe6
rae <rae@raeveira.nl>
2025-02-17 22:50:28 +0100

    added more logs

--- front-end/app/actions/auth.ts
+++ front-end/app/actions/auth.ts
@@ -1,5 +1,5 @@
 'use server'
-import { loginResponseSchema, loginSchema } from '@/lib/schemas';
+import {loginResponseSchema, loginSchema} from '@/lib/schemas';
 
 export async function loginUser(formData: FormData) {
     const emailOrUsername = formData.get('emailOrUsername') as string;
@@ -12,7 +12,7 @@ export async function loginUser(formData: FormData) {
     });
 
     try {
-        const body ={email: parsedFormData.emailOrUsername, password: parsedFormData.password};
+        const body = {email: parsedFormData.emailOrUsername, password: parsedFormData.password};
 
         const response = await fetch('https://tvytapi.raeveira.nl/api/auth/login', {
             method: 'POST',
@@ -23,18 +23,23 @@ export async function loginUser(formData: FormData) {
             body: JSON.stringify(body),
         });
 
+        console.log(response);
+
         if (response.ok) {
             const data = await response.json();
+
+            console.log(data); // This is not type-safe
+
             // Validate the response data
             const parsedResponse = loginResponseSchema.parse(data);
 
             console.log(parsedResponse.message); // Now this is type-safe
-            return { success: true };
+            return {success: true, data: parsedResponse};
         } else {
-            return { error: 'Invalid credentials' };
+            return {error: 'Invalid credentials'};
         }
     } catch (error) {
         console.error(error)
-        return { error: 'An error occurred. Please try again.' };
+        return {error: 'An error occurred. Please try again.'};
     }
 }
